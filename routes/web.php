04881ffd6e890b2cf5cb9c3ca83439f78cebc4fb
DaeJin Jeong <devwithu@naver.com>
2017-10-20 08:40:02 +0900

    Event to Boradcast

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('chat', function () {
-    return view('chat');
-});
+Route::get('chat', 'ChatController@chat');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
